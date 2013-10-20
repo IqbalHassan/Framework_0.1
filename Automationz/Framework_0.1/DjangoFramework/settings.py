16f3e04bcc485ab39e61b48024888108e1b3a440
riz-hossain <hossain.iqbal@gmail.com>
2013-10-20 17:15:35 -0400

    testing the remote IP addesss

--- Automationz/Framework_0.1/DjangoFramework/settings.py
+++ Automationz/Framework_0.1/DjangoFramework/settings.py
@@ -21,8 +21,8 @@ DATABASES = {
         'NAME': 'postgres', # Or path to database file if using sqlite3.
         'USER': 'postgres', # Not used with sqlite3.
         'PASSWORD': 'password', # Not used with sqlite3.
-        'HOST': '127.0.0.1', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+        'HOST': '135.23.123.67', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '8000', # Set to empty string for default. Not used with sqlite3.
     }
 }
 
