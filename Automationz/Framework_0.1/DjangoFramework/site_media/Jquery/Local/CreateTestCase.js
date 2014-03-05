d19d7309d4273bbdafdfc3abd0e55580a7fd33b1
Matiur Rahman Minar <minar09.bd@gmail.com>
2014-03-06 02:07:59 +0600

    timepicker not matching

--- Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/CreateTestCase.js
+++ Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/CreateTestCase.js
@@ -18,15 +18,6 @@ $(document).ready(function() {
     check_required_data();
     show_radio_button();
 
-    $('.timepicker2').timepicker({
-        minuteStep: 1,
-        template: 'dropdown',
-        appendWidgetTo: 'body',
-        showSeconds: true,
-        showMeridian: false,
-        defaultTime: false
-    });
-
     /*****************Shetu's Function************************/
     AutoCompleteTag();
     /*****************End Shetu************************/
@@ -941,13 +932,12 @@ function GenerateMainRow()
             '<td><textarea id="searchbox'+step_num+'expected" class="ui-corner-all  ui-autocomplete-input" style="width: 90%"></textarea></td>' +
             '<td><input type="checkbox" id="searchbox'+step_num+'verify" value="yes"></td>' +
             '<td><span id="searchbox'+step_num+'step_type"></span></td>' +
-            '<td><div class="input-append bootstrap-timepicker"><input id="searchbox'+step_num+'step_est_time" type="text" class="input-small timepicker2 textbox">' +
-            '<span class="add-on">' +
-            '<i class="icon-time"><img alt="Octocat-spinner-32" class="mini-loader" height="16" src="/site_media/clock.png" width="16" style="padding-top: 30%"></i></span></div></td>' +
+            //'<td><input class="new_tc_form est_time_img" type=\'image\' src=\'/site_media/clock.png\' style=\"background-color: transparent; width:16px; height:16px\"></td>' +
+            '<td><input type="time" step="1" name="time" id="searchbox'+step_num+'step_est_time" class="textbox" style="width: auto"></td>' +
             '<td><a id="searchbox'+step_num+'step_desc" class="descriptionpop notification-indicator tooltipped downwards" data-gotokey="n" style="cursor:pointer;"><span class="mail-status"></span></a></td>' +
             '<td><input class="new_tc_form add_after_img" type=\'image\' src=\'/site_media/new.png\' name=\'Add Step\' style=\"background-color: transparent; width:18px; height:18px\"></td>' +
             '</tr>'
-        );
+        )
     return message;
 }
 function GeneratePopUpMetaData(){
