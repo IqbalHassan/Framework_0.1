5920cdab6831a9ef6beda0f6133b07f67c659d5d
sazid <sazidozon@gmail.com>
2014-05-07 22:43:18 +0600

    Change to folder icon

--- Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/ManageTestCases.js
+++ Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/ManageTestCases.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
 				},
 				
 				"parent_section" : {
-					"icon" : "fa fa-th",
+					"icon" : "fa fa-folder",
 					"valid_children" : [ "section" ]
 				},
 				
