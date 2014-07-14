84e6c9e5c7d1f47b8ba13b9ee800e4dd112f72af
shetu <shetu2153@hotmail.com>
2014-07-14 07:49:12 +0600

    Dynamic Module is working.

--- Automationz/Framework_0.1/FutureShop/Drivers/Futureshop.py
+++ Automationz/Framework_0.1/FutureShop/Drivers/Futureshop.py
@@ -25,65 +25,119 @@ if os.name == 'posix':
     import MacCommonFoldersPaths as ComPath
     import Program_Mac as PIM
 
+def open_browser(conn,sModuleInfo,sClientName,add_find_SQLQuery,edit_SQLQuery):
+    CommonUtil.ExecLog(sModuleInfo, "Opening browser", 1)
+    browser = sClientName
+    #Adding working around for the current bug where we are not able to select browser
+    #browser = "Firefox"
+    sTestStepReturnStatus = WebProgram.BrowserSelection(browser)
+    print sTestStepReturnStatus
+    return sTestStepReturnStatus
 
 
+def go_to_webpage(conn,sModuleInfo,sClientName,add_find_SQLQuery,edit_SQLQuery):
+    DataSet = DBUtil.GetData(conn, add_find_SQLQuery, False)
+    web_link = DataSet[0][2]
+    sTestStepReturnStatus = WebProgram.OpenLink(web_link)
+    print sTestStepReturnStatus
+    return sTestStepReturnStatus
 
-def ExecuteTestSteps(conn, CurrentStep, TCID, sClientName, StepSeq, DataSet, q):
+
+def close_browser(conn,sModuleInfo,sClientName,add_find_SQLQuery,edit_SQLQuery):
+    CommonUtil.ExecLog(sModuleInfo, "skipping closing browser", 1)
+    sTestStepReturnStatus = WebProgram.CloseBrowser()
+    print sTestStepReturnStatus
+    return sTestStepReturnStatus
+
+#function for the query write
+#get the add_find_sql_query
+
+function_mappings={
+    'Open Browser':open_browser,
+    'Go to webpage':go_to_webpage,
+    'Close Browser':close_browser,
+    
+    #'Open Futureshop':
+    #'Select Main Menu':
+    #'Select Sub Menu 1':
+    #'Select Sub Menu 2':
+    #'Select Filter Checkbox':
+    #'Search For An Item':
+    #'Verify Filter Items Count':
+    #'Verify Product Details':
+    
+}
+
+def get_add_find_SQLQuery(TCID,StepSeq,DataSet,referred_run):
+    add_find_SQLQuery=("select pmd.id,pmd.field,pmd.value"
+                " from result_test_cases tc,result_test_case_datasets tcd,result_test_steps_data tsd,result_container_type_data ctd,result_master_data pmd"
+                " where tc.run_id=tcd.run_id"
+                " and tc.tc_id=tcd.tc_id"
+                " and tcd.run_id=tsd.run_id"
+                " and tcd.tcdatasetid=tsd.tcdatasetid"
+                " and tsd.testdatasetid=ctd.dataid"
+                " and tsd.run_id=ctd.run_id"
+                " and ctd.curname=pmd.id"
+                " and ctd.run_id=pmd.run_id"
+                " and tc.tc_id='%s'"
+                " and tcd.tcdatasetid='%s'"
+                " and tsd.teststepseq=%d"
+                " and tc.run_id='%s' order by pmd.id"% (TCID,DataSet, StepSeq, referred_run))
+    return add_find_SQLQuery
+
+def get_edit_SQLQuery(TCID,StepSeq,Dataset,referred_run):
+    edit_SQLQuery = ("select "
+            " ctd.curname,"
+            " ctd.newname"
+            " from result_Test_Steps tst, result_test_case_datasets tcd, result_test_steps_data tsd, result_container_type_data ctd"
+            " where"
+            " tst.tc_id = tcd.tc_id"
+            " and tst.run_id=tcd.run_id"
+            " and tcd.tcdatasetid = tsd.tcdatasetid"
+            " and tcd.run_id=tsd.run_id"
+            " and tst.teststepsequence = tsd.teststepseq"
+            " and tst.run_id=tsd.run_id"
+            " and tsd.testdatasetid = ctd.dataid"
+            " and tsd.run_id=ctd.run_id"
+            " and tst.tc_id = '%s'"
+            " and tst.teststepsequence = '%s'"
+            " and tcd.tcdatasetid = '%s'"
+            " and tst.run_id='%s'" % (TCID, StepSeq, Dataset,referred_run))
+    return edit_SQLQuery    
+
+def ExecuteTestSteps(conn, CurrentStep, TCID, sClientName, StepSeq, DataSet, q,run_id):
     sModuleInfo = inspect.stack()[0][3] + " : " + inspect.getmoduleinfo(__file__).name
     try:
-
-        add_find_SQLQuery = ("select "
-        " pmd.id,"
-        " pmd.field,"
-        " pmd.value"
-        " from Test_Steps tst, test_case_datasets tcd, test_steps_data tsd, container_type_data ctd, master_data pmd"
-        " where"
-        " tst.tc_id = tcd.tc_id"
-        " and tcd.tcdatasetid = tsd.tcdatasetid"
-        " and tst.teststepsequence = tsd.teststepseq"
-        " and tsd.testdatasetid = ctd.dataid"
-        " and ctd.curname = pmd.id"
-        " and tst.tc_id = '%s'"
-        " and tst.teststepsequence = '%s'"
-        " and tcd.tcdatasetid = '%s' order by pmd.id;" % (TCID, StepSeq, DataSet))
-
-        edit_SQLQuery = ("select "
-        " ctd.curname,"
-        " ctd.newname"
-        " from Test_Steps tst, test_case_datasets tcd, test_steps_data tsd, container_type_data ctd"
-        " where"
-        " tst.tc_id = tcd.tc_id"
-        " and tcd.tcdatasetid = tsd.tcdatasetid"
-        " and tst.teststepsequence = tsd.teststepseq"
-        " and tsd.testdatasetid = ctd.dataid"
-        " and tst.tc_id = '%s'"
-        " and tst.teststepsequence = '%s'"
-        " and tcd.tcdatasetid = '%s';" % (TCID, StepSeq, DataSet))
-
-        exp_SQLQuery = ("select "
-        " pmd.id,"
-        " pmd.field,"
-        " pmd.value"
-        " from test_case_datasets tcd, expected_datasets ed, expected_container ec, container_type_data ctd, master_data pmd"
-        " where"
-        " tcd.tcdatasetid = ed.datasetid"
-        " and ed.expectedrefid = ec.exprefid"
-        " and ec.container_name = ctd.dataid"
-        " and ctd.curname = pmd.id"
-        " and ed.stepsseq = '%s'"
-        " and tcd.tcdatasetid = '%s' order by pmd.id;" % (StepSeq, DataSet))
-
-        exp_PerfQuery = ("select "
-        " ec.container_name"
-        " from test_case_datasets tcd, expected_datasets ed, expected_container ec"
-        " where"
-        " tcd.tcdatasetid = ed.datasetid"
-        " and ed.expectedrefid = ec.exprefid"
-        " and ed.stepsseq = '%s'"
-        " and tcd.tcdatasetid = '%s';" % (StepSeq, DataSet))
-
-
-        if CurrentStep == "Open Browser":
+        
+        add_find_SQLQuery=get_add_find_SQLQuery(TCID, StepSeq, DataSet,run_id)
+        edit_SQLQuery=get_edit_SQLQuery(TCID, StepSeq, DataSet,run_id)
+        """if RerunTag==False:
+                
+            
+    
+            exp_SQLQuery = ("select "
+            " pmd.id,"
+            " pmd.field,"
+            " pmd.value"
+            " from test_case_datasets tcd, expected_datasets ed, expected_container ec, container_type_data ctd, master_data pmd"
+            " where"
+            " tcd.tcdatasetid = ed.datasetid"
+            " and ed.expectedrefid = ec.exprefid"
+            " and ec.container_name = ctd.dataid"
+            " and ctd.curname = pmd.id"
+            " and ed.stepsseq = '%s'"
+            " and tcd.tcdatasetid = '%s' order by pmd.id;" % (StepSeq, DataSet))
+    
+            exp_PerfQuery = ("select "
+            " ec.container_name"
+            " from test_case_datasets tcd, expected_datasets ed, expected_container ec"
+            " where"
+            " tcd.tcdatasetid = ed.datasetid"
+            " and ed.expectedrefid = ec.exprefid"
+            " and ed.stepsseq = '%s'"
+            " and tcd.tcdatasetid = '%s';" % (StepSeq, DataSet))
+"""                
+        """if CurrentStep == "Open Browser":
             CommonUtil.ExecLog(sModuleInfo, "Opening browser", 1)
             browser = sClientName
             #Adding working around for the current bug where we are not able to select browser
@@ -199,7 +253,13 @@ def ExecuteTestSteps(conn, CurrentStep, TCID, sClientName, StepSeq, DataSet, q):
             sTestStepReturnStatus = "Not Run"
             print "Framework error: Unable to locate the step driver for ", CurrentStep
             CommonUtil.ExecLog(sModuleInfo, "Unknown test step : %s" % CurrentStep , 2)
-
+"""
+        for each in function_mappings.keys():
+            if CurrentStep==each:
+                function_name=function_mappings[each]
+                #things needed to generalize
+                sTestStepReturnStatus=function_name(conn,sModuleInfo,sClientName,add_find_SQLQuery,edit_SQLQuery)
+                break
     except Exception, e:
         print "Exception : ", e
         CommonUtil.ExecLog(sModuleInfo, "Exception : %s" % e , 3)
