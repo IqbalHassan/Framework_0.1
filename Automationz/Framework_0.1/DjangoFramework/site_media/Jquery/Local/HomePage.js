6172a854e2784131e0520e5a5e55411a0fe366ad
Matiur Rahman Minar <minar09.bd@gmail.com>
2014-08-14 15:22:08 +0600

    Dashboard has no problem, so it'll be fine. Things are better now, Alhamdulillah.

--- Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/HomePage.js
+++ Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/HomePage.js
@@ -3,43 +3,48 @@
  */
 $(document).ready(function(){
 
-    //var user = $(".welcome").text();
+    var user = $.session.get('fullname');
     $(".logged_user").text($.session.get('fullname'));
-    $(".test_count").text($.session.get('testing'));
-    $(".require_count").text($.session.get('requires'));
+    //$(".test_count").text($.session.get('testing'));
+    //$(".require_count").text($.session.get('requires'));
 
     /*$("#GetAssignedTests").click(function(){
-        $.get("GetAssignedTasks",{user : user},function(data)
-        {
-            //$("#test_count").text(data['TableData'].length);
-            ResultTable(ass_tasks,data['Heading'],data['TableData'],"Assigned Tasks");
-        });
-    });*/
+     $.get("GetAssignedTasks",{user : user},function(data)
+     {
+     //$("#test_count").text(data['TableData'].length);
+     ResultTable(ass_tasks,data['Heading'],data['TableData'],"Assigned Tasks");
+     });
+     });*/
 
-    /*$.get("GetAssignedTests",{user : user},function(data)
+    $.get("GetAssignedTests",{user : user},function(data)
+     {
+     $(".test_count").text(data['TableData'].length);
+     //ResultTable(ass_tasks,data['Heading'],data['TableData'],"Assigned Tasks");
+     });
+
+    $.get("GetRequirements",{user : user},function(data)
     {
-        $("#test_count").text(data['TableData'].length);
-        //ResultTable(ass_tasks,data['Heading'],data['TableData'],"Assigned Tasks");
-    });*/
+        $(".require_count").text(data['TableData'].length);
+    });
 
     /*$("#assigned_tasks").click(function(){
-        $("#task_prompt").html(
-            '<p style="text-align: center">You have selected ' +
-                tc_id +'-'+ tc_name + '.' +
-                '<br/> What do you want to do?' +
-                '</p>' +
-                '<div style="padding-left: 15%">' +
-                '<a class="github" href="/Home/ManageTestCases/Edit/'+tc_id+'">Edit</a>' +
-                '<a class="twitter" href="/Home/ManageTestCases/CreateNew/'+tc_id+'">Copy</a>' +
-                '<a class="dribble" href="#" rel="modal:close">Close</a>' +
-                '</div>'
+     $("#task_prompt").html(
+     '<p style="text-align: center">You have selected ' +
+     tc_id +'-'+ tc_name + '.' +
+     '<br/> What do you want to do?' +
+     '</p>' +
+     '<div style="padding-left: 15%">' +
+     '<a class="github" href="/Home/ManageTestCases/Edit/'+tc_id+'">Edit</a>' +
+     '<a class="twitter" href="/Home/ManageTestCases/CreateNew/'+tc_id+'">Copy</a>' +
+     '<a class="dribble" href="#" rel="modal:close">Close</a>' +
+     '</div>'
 
-        );
-        $("#task_prompt").modal();
-        $.get("GetAssignedTests",{user : user},function(data)
-        {
-            ResultTable(task_prompt,data['Heading'],data['TableData'],"Assigned Tasks");
+     );
+     $("#task_prompt").modal();
+     $.get("GetAssignedTests",{user : user},function(data)
+     {
+     ResultTable(task_prompt,data['Heading'],data['TableData'],"Assigned Tasks");
 
-        });
-    });*/
+     });
+     });*/
 });
