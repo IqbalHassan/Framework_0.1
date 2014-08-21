a3078a54997c41ba2c578db45b5b5fae609a741e
Matiur Rahman Minar <minar09.bd@gmail.com>
2014-08-21 15:37:25 +0600

    Manage Milestone page reformed!

--- Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/Milestone.js
+++ Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/Milestone.js
@@ -1,16 +1,179 @@
 /**
  * Created by lent400 on 5/21/14.
  */
+var operation = 1;
 $(document).ready(function(){
-   MileStoneTab();
+   //MileStoneTab();
 
-    $("#show_all_ms").click(function(){
-        $.get("GetMileStones",{term : ''},function(data)
-        {
-            ResultTable(all_milestones,data['Heading'],data['TableData'],"Milestones");
-        });
-    });
+    New_UI();
 });
+
+function New_UI(){
+    status_button_preparation();
+
+    $.get("GetMileStones",{term : ''},function(data)
+    {
+        ResultTable(all_milestones,data['Heading'],data['TableData'],"Milestones");
+    });
+
+    $('#starting_date').datepicker({ dateFormat: "yy-mm-dd" });
+    $('#ending_date').datepicker({ dateFormat: "yy-mm-dd" });
+
+    $('#msinput').autocomplete({
+        source:function(request,response){
+            if($('#operation_milestone option:selected').val()!="0"){
+                $.ajax({
+                    url:"AutoMileStone",
+                    dataType:"json",
+                    data:{term:request.term},
+                    success:function(data){
+                        response(data);
+
+                    }
+                });
+            }
+        },
+        select:function(request,ui){
+            var value=ui.item[0].trim();
+            if (value!=""){
+                $(this).val(value.trim());
+                operation = 2;
+                $("#renamebox").show();
+                $("#starting_date").val(ui.item[1]);
+                $("#ending_date").val(ui.item[2]);
+                return false;
+            }
+        }
+    }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+        return $( "<li></li>" )
+            .data( "ui-autocomplete-item", item )
+            .append( "<a><strong>" + item[0] + "</strong> - "+item[1]+ " - " + item[2] +"</a>" )
+            .appendTo( ul );
+    };
+
+
+    $('#msinput2').autocomplete({
+        source:function(request,response){
+            if($('#operation_milestone option:selected').val()!="0"){
+                $.ajax({
+                    url:"AutoMileStone",
+                    dataType:"json",
+                    data:{term:request.term},
+                    success:function(data){
+                        response(data);
+                    }
+                });
+            }
+        },
+        select:function(request,ui){
+            var value=ui.item[0].trim();
+            if (value!=""){
+                $(this).val(value.trim());
+                return false;
+            }
+        }
+    }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+        return $( "<li></li>" )
+            .data( "ui-autocomplete-item", item )
+            .append( "<a><strong>" + item[0] + "</strong> - "+item[1]+"</a>" )
+            .appendTo( ul );
+    };
+    
+    $('#ms_button').live('click',function(){
+       // var operation=$('#operation_milestone option:selected').val();
+        if(operation==1){
+            var new_name=$('#msinput').val();
+            var start_date = $("#starting_date").val();
+            var end_date = $("#ending_date").val();
+        }
+        else if(operation==2){
+            var new_name=$('#msinput2').val();
+            var old_name=$('#msinput').val();
+            var start_date = $("#starting_date").val();
+            var end_date = $("#ending_date").val();
+        }
+        else{
+            var new_name=$('#msinput').val();
+            var old_name="";
+            var start_date = "";
+            var end_date = "";
+        }
+        if(operation=="0"){
+            var title_msg = "Fields are Empty!"
+        }
+        else if(operation=="1"){
+            title_msg = "Milestone Created"
+        }
+        else if(operation=="2"){
+            title_msg = "Milestone Modified"
+        }
+        else if(operation=="3"){
+            title_msg = "Milestone Deleted"
+        }
+        if(operation=="0"||new_name==''){
+            var error_message="<b style='color: #ff0000'>Fields are empty</b>";
+            alertify.log("Fields are empty","",0);
+            $('#error_milestone').html(error_message);
+            $('#error_milestone').css({'display':'block'});
+        }
+        else {
+            $.get('MileStoneOperation',{old_name:old_name,new_name:new_name,operation:operation,start_date:start_date.trim(),end_date:end_date.trim()},function(data){
+                if(data['confirm_message']==""){
+                    var color='red';
+                }
+                else{
+                    var color='green';
+                }
+                if(data['confirm_message']==""){
+                    alertify.log(""+data['error_message']+"","",0);
+                    milestone_notify(""+data['error_message']+"",""+data['error_message']+"","/site_media/noti2.ico");
+                    $('#error_milestone').html('<b style="color:red;">'+data['error_message']+'<br>Page will be refreshed in 3 seconds to change effect</b>');
+                    $('#error_milestone').slideDown('slow');
+                    //setTimeout(function(){window.location='/Home/RunTest/';},4000);
+                    window.location = '/Home/ManageMilestone/';
+                }
+                else{
+                    alertify.log(""+data['confirm_message']+"","",0);
+                    milestone_notify(title_msg,""+data['confirm_message']+"","/site_media/noti.ico");
+                    $('#error_milestone').html('<b style="color:green;">'+data['confirm_message']+'<br>Page will be refreshed in 3 seconds to change effect</b>');
+                    $('#error_milestone').slideDown('slow');
+                    //setTimeout(function(){window.location='/Home/RunTest/';},4000);
+                    window.location = '/Home/ManageMilestone/';
+                }
+
+            })
+        }
+    });
+
+}
+
+function status_button_preparation(){
+    $("#not_started").click(function(){
+        $(this).addClass("selected");
+        $('#started').removeClass("selected");
+        $('#over_due').removeClass("selected");
+        $('#complete').removeClass("selected");
+    });
+    $("#started").click(function(){
+        $(this).addClass("selected");
+        $('#not_started ').removeClass("selected");
+        $('#over_due').removeClass("selected");
+        $('#complete').removeClass("selected");
+    });
+    $("#over_due").click(function(){
+        $(this).addClass("selected");
+        $('#complete').removeClass("selected");
+        $('#not_started').removeClass("selected");
+        $('#started').removeClass("selected");
+    });
+    $("#complete").click(function(){
+        $(this).addClass("selected");
+        $('#started').removeClass("selected");
+        $('#over_due').removeClass("selected");
+        $('#not_started').removeClass("selected");
+    });
+}
+
 function MileStoneTab(){
     $('#msinput').autocomplete({
         source:function(request,response){
