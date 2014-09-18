fcd0236ddbc525f083beb5d243ae2e50b1197b50
Mohammed Sazid-Al-Rashid <sazidozon@gmail.com>
2014-09-18 11:58:39 +0600

    Implement menu bar with Sub-levels #322
    Alongside the implementation of the new menu bar, some of the old icons
    have been replaced. Fontawesome, the icon library has also been updated.
    
    The Project & Team Selection 'selects' won't work here. They are just
    placeholders. Someone needs to update these two placeholders.
    
    @riz-hossain  @minar09  @Taitalus  Please test and report.

--- Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/Base.js
+++ Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/Base.js
@@ -1,8 +1,81 @@
 /**
  * Last modified by: sazid on 4/25/14.
  */
+
+/******** For menu bar *********/
+(function($) {
+
+  $.fn.menumaker = function(options) {
+      
+      var cssmenu = $(this), settings = $.extend({
+        title: "Menu",
+        format: "dropdown",
+        sticky: false
+      }, options);
+
+      return this.each(function() {
+    	/*
+        cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+        $(this).find("#menu-button").on('click', function(){
+          $(this).toggleClass('menu-opened');
+          var mainmenu = $(this).next('ul');
+          if (mainmenu.hasClass('open')) { 
+            mainmenu.hide().removeClass('open');
+          }
+          else {
+            mainmenu.show().addClass('open');
+            if (settings.format === "dropdown") {
+              mainmenu.find('ul').show();
+            }
+          }
+        });
+        */
+
+        cssmenu.find('li ul').parent().addClass('has-sub');
+
+        multiTg = function() {
+          cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+          cssmenu.find('.submenu-button').on('click', function() {
+            $(this).toggleClass('submenu-opened');
+            if ($(this).siblings('ul').hasClass('open')) {
+              $(this).siblings('ul').removeClass('open').hide();
+            }
+            else {
+              $(this).siblings('ul').addClass('open').show();
+            }
+          });
+        };
+
+        if (settings.format === 'multitoggle') multiTg();
+        else cssmenu.addClass('dropdown');
+
+        if (settings.sticky === true) cssmenu.css('position', 'fixed');
+
+        resizeFix = function() {
+          if ($( window ).width() > 768) {
+            cssmenu.find('ul').show();
+          }
+
+          if ($(window).width() <= 768) {
+            cssmenu.find('ul').hide().removeClass('open');
+          }
+        };
+        resizeFix();
+        return $(window).on('resize', resizeFix);
+
+      });
+  };
+})(jQuery);
+
+/**********************************/
+
 $(document).ready(function(){
 
+	$("#cssmenu").menumaker({
+	   title: "Menu",
+	   format: "multitoggle"
+	});
+	
 	// Remember the path so that after the login, the user is redirected back to the same page he/she was trying to view
 	current_path = window.location.pathname;
 	// Save it to session storage rather than local storage
