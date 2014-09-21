633f436b65711447a3f23a0fd44571bf41569f4a
Raju Ahmed <shetu2153@gmail.com>
2014-09-21 15:52:27 +0600

    Finalized the AssignSettings page .needs some border removing in the version tab

--- Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/AssignSettings.js
+++ Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/AssignSettings.js
@@ -118,7 +118,64 @@ function get_all_dependency(project_id,team_id){
         $('#team_id').html('<b>'+project_id+'<br>'+team_name+'</b>');
         $('#dependency_tab').html(populate_upper_div(data['dependency_list']));
         $('#unused_tab').html(populate_lower_div(data['unused_list']));
+        $('.dependency').on('click',function(){
+            $('.dependency').css({'background':'none'});
+            $('.dependency').removeClass('selected');
+            $(this).css({'background':'#ccc'});
+            $(this).addClass('selected');
+            var value=$(this).attr('value');
+            $.get('get_all_names_dependency',{
+               'project_id':project_id,
+                'team_id':team_id,
+                'selected_dependency':value
+            },function(data){
+                $('#name_tab').attr('value',value);
+                $('#bit_version').html("");
+                var name_list=data['name_list'];
+                $('#name_tab').html(populate_name_div(name_list));
+                $('.name').click(function(){
+                    $('.name').css({'background':'none'});
+                    $(this).css({'background':'#ccc'});
+                    var value=$(this).attr('value');
+                    $.get('get_all_version_dependency',{
+                        'project_id':project_id,
+                        'team_id':team_id,
+                        'selected_dependency':$('#name_tab').attr('value'),
+                        'selected_name':value
+                    },function(data){
+                        var name_list=data['version_list'];
+                        $('#bit_version').html(populate_version_list(name_list));
+
+                    });
+                });
+            });
+        });
     });
+}
+function populate_version_list(array_list){
+    var message="";
+    message+='<table width="100%" class="one-column-emphasis">';
+    for(var i=0;i<array_list.length;i++){
+        message+='<tr><td  style="cursor:pointer;">'+array_list[i][0]+'</td>';
+        message+='<td><table width="100%" style="border-top: none;">';
+        var version_list=array_list[i][1].split(",");
+        for(var j=0;j<version_list.length;j++){
+            message+='<tr><td>'+version_list[j]+'</td></tr>';
+        }
+        message+='</table></td>'
+        message+='</tr>';
+    }
+    message+='</table>';
+    return message;
+}
+function populate_name_div(array_list){
+    var message="";
+    message+='<table width="100%" class="one-column-emphasis">';
+    for(var i=0;i<array_list.length;i++){
+        message+=('<tr><td  style="cursor:pointer;" class="name" value="'+array_list[i][0]+'">'+array_list[i][1]+'</td></tr>');
+    }
+    message+='</table>';
+    return message;
 }
 function populate_lower_div(array_list){
     var message="";
@@ -136,7 +193,7 @@ function populate_upper_div(array_list){
     var message="";
     message+='<table width="100%" class="one-column-emphasis">';
     for(var i=0;i<array_list.length;i++){
-        message+=('<tr><td class="dependency" value="'+array_list[i][0]+'">'+array_list[i][1]+'</td></tr>');
+        message+=('<tr><td  style="cursor:pointer;" class="dependency" value="'+array_list[i][0]+'">'+array_list[i][1]+'</td></tr>');
     }
     message+='</table>';
     return message;
