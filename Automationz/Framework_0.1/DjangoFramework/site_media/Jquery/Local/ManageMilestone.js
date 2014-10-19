a1def0adc97025368c99980563819732f2cebeb3
Matiur Rahman Minar <minar09.bd@gmail.com>
2014-10-20 02:47:48 +0600

    milestones view as results page!

--- Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/ManageMilestone.js
+++ Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/ManageMilestone.js
@@ -3,4 +3,48 @@
  */
 $(document).ready(function(){
 
+    $("#simple-menu").sidr({
+        name: 'sidr',
+        side: 'left'
+    });
+
+    $.get("GetMileStones",{term : ''},function(data)
+    {
+        if(data['TableData'].length>0) {
+            //make a table column
+            var message = "";
+            message += '<table class="one-column-emphasis">';
+            message += '<tr>';
+            for (var i = 0; i < data['Heading'].length; i++) {
+                message += '<th align="left">' + data['Heading'][i] + '</th>';
+            }
+            message += '</tr>'
+            for (var i = 0; i < data['TableData'].length; i++) {
+                message += '<tr>';
+                for (var j = 0; j < data['TableData'][i].length; j++) {
+                    message += '<td align="left">' + data['TableData'][i][j] + '</td>';
+
+
+                }
+                message += '</tr>';
+            }
+            message += '</table>';
+            $('#allMilestones').html(message);
+            make_clickable('#allMilestones');
+
+        }
+    });
+
+
 });
+
+
+function make_clickable(divname) {
+    $(divname + ' tr>td:first-child').each(function () {
+        $(this).css({
+            'color': 'blue',
+            'cursor': 'pointer',
+            'textAlign': 'left'
+        });
+    });
+}
