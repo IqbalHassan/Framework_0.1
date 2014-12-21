6a5283c48c14584d4dd6a6b8eed38cccde85dac8
Matiur Rahman Minar <minar09.bd@gmail.com>
2014-12-21 13:39:34 +0600

    task-test case linking so far, searching with section is not fully done yet, it just shows.

--- Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/CreateNewTask.js
+++ Automationz/Framework_0.1/DjangoFramework/site_media/Jquery/Local/CreateNewTask.js
@@ -58,7 +58,7 @@ function Suggestion(project_id,team_id){
         {
             source : function(request, response) {
                 $.ajax({
-                    url:"AutoCompleteTestCasesSearchOtherPages",
+                    url:"AutoCompleteTestCasesSearchOtherPages/",
                     dataType: "json",
                     data:{ term: request.term,project_id:project_id,team_id:team_id},
                     success: function( data ) {
@@ -189,63 +189,12 @@ function TestCaseLinking(){
         $(this).remove();
 
     });
-
-
-    $("#searchbox").autocomplete(
-        {
-            source : function(request, response) {
-                $.ajax({
-                    url:"AutoCompleteTestCasesSearchOtherPages",
-                    dataType: "json",
-                    data:{ term: request.term,project_id:project_id,team_id:team_id},
-                    success: function( data ) {
-                        response( data );
-                    }
-                });
-            },
-
-            //source : 'AutoCompleteTestCasesSearch?Env = ' +Env,
-            select : function(event, ui) {
-
-                var tc_id_name = ui.item[0].split(" - ");
-                var value = "";
-                if (tc_id_name != null)
-                    value = tc_id_name[0].trim();
-
-                if(value != "")
-                {
-                    $("#searchedFilter").append('<td><img class="delete" title = "Delete" src="/site_media/deletebutton.png" /></td>'
-                        + '<td name = "submitquery" class = "Text" style = "size:10">'
-                        + value
-                        + ":&nbsp"
-                        + '</td>'
-                    );
-                    PerformSearch(project_id,team_id);
-                }
-                $("#searchbox").val("");
-                return false;
-            }
-        }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-        return $( "<li></li>" )
-            .data( "ui-autocomplete-item", item )
-            .append( "<a>" + item[0] + "<strong> - " + item[1] + "</strong></a>" )
-            .appendTo( ul );
-    };
-
-    $("#searchbox").keypress(function(event) {
-        if (event.which == 13) {
-
-            event.preventDefault();
-
-        }
-    });
-
 }
 function PerformSearch(project_id,team_id){
     $("#searchedFilter").each(function() {
         var UserText = $(this).find("td").text();
         UserText = UserText.replace(/(\r\n|\n|\r)/gm, "").replace(/^\s+/g, "")
-        $.get('TableDataTestCasesOtherPages',{Query:UserText,test_status_request:false,project_id:project_id,team_id:team_id},function(data){
+        $.get('TableDataTestCasesOtherPages/',{Query:UserText,test_status_request:false,project_id:project_id,team_id:team_id},function(data){
             if(data['TableData'].length!=0){
                 ResultTable("#RunTestResultTable",data['Heading'],data['TableData'],'Test Cases');
                 implementDropDown("#RunTestResultTable");
@@ -585,6 +534,51 @@ function Submit_button_preparation(){
         }
     });
 }
+
+function implementDropDown(wheretoplace){
+    $(wheretoplace+" tr td:nth-child(2)").css({'color' : 'blue','cursor' : 'pointer'});
+    $(wheretoplace+" tr td:nth-child(2)").each(function() {
+        var ID=$(this).closest('tr').find('td:nth-child(1)').text().trim();
+        var name=$(this).text().trim();
+        $(this).html('<div id="'+ID+'name">'+name+'</div><div id="'+ID+'detail" style="display:none;"></div>');
+        $.get("TestStepWithTypeInTable",{RunID: ID},function(data) {
+            var data_list=data['Result'];
+            var column=data['column'];
+            ResultTable(wheretoplace+ ' #'+ID+'detail',column,data_list,"");
+            $(wheretoplace+' #'+ID+'detail tr').each(function(){
+                $(this).css({'textAlign':'left'});
+            });
+        });
+        $(this).live('click',function(){
+            $(wheretoplace+' #'+ID+'detail').slideToggle("slow");
+        });
+    });
+}
+
+function Buttons(type,name){
+    $('#add_button').click(function(event){
+        event.preventDefault();
+        var list=[]
+        $('.add:checked').each(function(){
+            list.push($(this).attr('id').trim());
+        });
+        if(list.length==0){
+            alert('No Test Case selected');
+            return false;
+        }
+        else{
+            $.get('AddTestCasesSetTag',{type:type.toLocaleUpperCase().trim(),name:name.trim(),list:list.join('|')},function(data){
+                alertify.success(data,"",3);
+                var location='/Home/ManageSetTag/'+type+'/'+name+'/';
+                window.location=location;
+            });
+
+        }
+
+    });
+
+}
+
 /*function status_button_preparation(){
     $("#not_started").click(function(){
         $(this).addClass("selected");
