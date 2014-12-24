eb1eaa8607da4ad728b7d8bc43630aebc7d481dd
Raju Ahmed <shetu2153@gmail.com>
2014-12-24 22:03:04 +0600

    @minar09 my pleasure.

--- Automationz/Framework_0.1/DjangoFramework/MySite/views.py
+++ Automationz/Framework_0.1/DjangoFramework/MySite/views.py
@@ -11117,7 +11117,7 @@ def get_default_settings(request):
                     Conn.close()
                     temp=[]
                     if isinstance(default_choices,list):
-                        if default_choices[0]!=None:
+                        if default_choices[0]!='':
                             for eachitem in default_choices[0].split(","):
                                 query="select name from dependency_name where id=%d and dependency_id=%d"%(int(eachitem),int(each[0]))
                                 Conn=GetConnection()
