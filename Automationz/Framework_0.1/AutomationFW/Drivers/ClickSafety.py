3630b325391ada868ee1bb50f5ee46e0cdb2cb63
riz-hossain <hossain.iqbal@gmail.com>
2015-02-07 12:52:23 -0500

    - Adding new driver and supporting files

--- Automationz/Framework_0.1/AutomationFW/Drivers/ClickSafety.py
+++ Automationz/Framework_0.1/AutomationFW/Drivers/ClickSafety.py
@@ -2,42 +2,93 @@
 import SeleniumScript
 
 def start_browser(dependency,step_data):
-    sClientName=dependency['Browser']
-    sTestStepReturnStatus = SeleniumScript.BrowserSelection(sClientName)
-    print sTestStepReturnStatus
-    return sTestStepReturnStatus
+    try:
+        sClientName=dependency['Browser']
+        sTestStepReturnStatus = SeleniumScript.BrowserSelection(sClientName)
+        print sTestStepReturnStatus
+        return sTestStepReturnStatus
+    except:
+        sTestStepReturnStatus = "Failed"
+        return
 
 def go_to_a_weblink(dependency,step_data):
-    first_data_set=step_data[0]
-    web_link=first_data_set[0][2]
-    title=first_data_set[0][2]
-    sTestStepReturnStatus = SeleniumScript.OpenLink(web_link, title)
-    print sTestStepReturnStatus
-    return sTestStepReturnStatus
+    try:
+        first_data_set=step_data[0]
+        web_link=first_data_set[0][2]
+        title=first_data_set[0][2]
+        sTestStepReturnStatus = SeleniumScript.OpenLink(web_link, title)
+        print sTestStepReturnStatus
+        return sTestStepReturnStatus
+    except:
+        sTestStepReturnStatus = "Failed"
+        return
 
 def login(dependency,step_data):
-    first_data_set=step_data[0]
-    user_name = first_data_set[0][2]
-    password = first_data_set[1][2]
-    sTestStepReturnStatus = SeleniumScript.Login(user_name,password)
-    print sTestStepReturnStatus
-    return sTestStepReturnStatus
+    try:        
+        first_data_set=step_data[0]
+        user_name = first_data_set[0][2]
+        password = first_data_set[1][2]
+        sTestStepReturnStatus = SeleniumScript.Login(user_name,password)
+        print sTestStepReturnStatus
+        return sTestStepReturnStatus
+    except:
+        sTestStepReturnStatus = "Failed"
+        return
     
 def expand_menu_by_name_or_id(dependency,step_data):
-    first_data_set=step_data[0]
-    menu_name=first_data_set[0][2]
-    sTestStepReturnStatus = SeleniumScript.Expand_Menu_By_Name_OR_ID(menu_name)
-    print sTestStepReturnStatus
-    return sTestStepReturnStatus       
+    try:        
+        first_data_set=step_data[0]
+        menu_name=first_data_set[0][2]
+        sTestStepReturnStatus = SeleniumScript.Expand_Menu_By_Name_OR_ID(menu_name)
+        print sTestStepReturnStatus
+        return sTestStepReturnStatus       
+    except:
+        sTestStepReturnStatus = "Failed"
+        return
 
 def click_element_by_name_or_id(dependency,step_data):
-    first_data_set=step_data[0]
-    element_name=first_data_set[0][2]
-    sTestStepReturnStatus = SeleniumScript.Click_Element_By_Name_OR_ID(element_name)
-    print sTestStepReturnStatus
-    return sTestStepReturnStatus  
+    try:        
+        first_data_set=step_data[0]
+        element_name=first_data_set[0][2]
+        sTestStepReturnStatus = SeleniumScript.Click_Element_By_Name_OR_ID(element_name)
+        print sTestStepReturnStatus
+        return sTestStepReturnStatus  
+    except:
+        sTestStepReturnStatus = "Failed"
+        return
 
-def close_browser(dependency,step_data):
-    sTestStepReturnStatus = SeleniumScript.CloseBrowser()
-    print sTestStepReturnStatus
-    return sTestStepReturnStatus
+def course_settings_time_limit(dependency,step_data):
+    try:        
+        first_data_set=step_data[0]
+        id_minlimit=first_data_set[0][0]
+        minlimit_value =first_data_set[0][2]
+        id_dailylimit=first_data_set[1][0]
+        dailylimit_value =first_data_set[1][2]
+        id_submitbutton =first_data_set[2][2]  
+        sTestStepReturnStatus = SeleniumScript.Course_Settings_Time_Limit(id_minlimit,minlimit_value,id_dailylimit,dailylimit_value,id_submitbutton)
+        print sTestStepReturnStatus
+        return sTestStepReturnStatus  
+    except:
+        sTestStepReturnStatus = "Failed"
+        return
+
+def verify_text_message_by_class(dependency,step_data):
+    try:        
+        first_data_set=step_data[0]
+        message=first_data_set[0][0]
+        expected_text =first_data_set[0][2]
+        sTestStepReturnStatus = SeleniumScript.Verify_Text_Message_By_Class(message,expected_text)
+        print sTestStepReturnStatus
+        return sTestStepReturnStatus 
+    except:
+        sTestStepReturnStatus = "Failed"
+        return
+
+def tear_down(dependency,step_data):
+    try:        
+        sTestStepReturnStatus = SeleniumScript.Tear_Down()
+        print sTestStepReturnStatus
+        return sTestStepReturnStatus
+    except:
+        sTestStepReturnStatus = "Failed"
+        return
