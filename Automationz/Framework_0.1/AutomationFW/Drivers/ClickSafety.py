f09f46b9869dc8cd63eabe512528665aba81d3fd
Riz Hossain <hossain.iqbal@gmail.com>
2015-02-14 15:26:24 -0500

    - some more reference update..

--- Automationz/Framework_0.1/AutomationFW/Drivers/ClickSafety.py
+++ Automationz/Framework_0.1/AutomationFW/Drivers/ClickSafety.py
@@ -1,10 +1,11 @@
 
-from Web import SeleniumScript
+#from Web import SeleniumScript
+import Web
 
 def start_browser(dependency,step_data):
     try:
         sClientName=dependency['Browser']
-        sTestStepReturnStatus = SeleniumScript.BrowserSelection(sClientName)
+        sTestStepReturnStatus = Web.SeleniumScript.BrowserSelection(sClientName)
         print sTestStepReturnStatus
         return sTestStepReturnStatus
     except:
@@ -16,7 +17,7 @@ def go_to_a_weblink(dependency,step_data):
         first_data_set=step_data[0]
         web_link=first_data_set[0][1]
         title=first_data_set[1][1]
-        sTestStepReturnStatus = SeleniumScript.OpenLink(web_link, title)
+        sTestStepReturnStatus = Web.SeleniumScript.OpenLink(web_link, title)
         print sTestStepReturnStatus
         return sTestStepReturnStatus
     except:
@@ -28,7 +29,7 @@ def log_in_to_clicksafety(dependency,step_data):
         first_data_set=step_data[0]
         user_name = first_data_set[0][1]
         password = first_data_set[1][1]
-        sTestStepReturnStatus = SeleniumScript.Login(user_name,password)
+        sTestStepReturnStatus = Web.SeleniumScript.Login(user_name,password)
         print sTestStepReturnStatus
         return sTestStepReturnStatus
     except:
@@ -39,7 +40,7 @@ def expand_menu_by_name_or_id(dependency,step_data):
     try:        
         first_data_set=step_data[0]
         menu_name=first_data_set[0][1]
-        sTestStepReturnStatus = SeleniumScript.Expand_Menu_By_Name_OR_ID(menu_name)
+        sTestStepReturnStatus = Web.SeleniumScript.Expand_Menu_By_Name_OR_ID(menu_name)
         print sTestStepReturnStatus
         return sTestStepReturnStatus       
     except:
@@ -50,7 +51,7 @@ def click_element_by_name_or_id(dependency,step_data):
     try:        
         first_data_set=step_data[0]
         element_name=first_data_set[0][1]
-        sTestStepReturnStatus = SeleniumScript.Click_Element_By_Name_OR_ID(element_name)
+        sTestStepReturnStatus = Web.SeleniumScript.Click_Element_By_Name_OR_ID(element_name)
         print sTestStepReturnStatus
         return sTestStepReturnStatus  
     except:
@@ -65,7 +66,7 @@ def course_settings_time_limit(dependency,step_data):
         id_dailylimit=first_data_set[1][0]
         dailylimit_value =first_data_set[1][2]
         id_submitbutton =first_data_set[2][2]  
-        sTestStepReturnStatus = SeleniumScript.Course_Settings_Time_Limit(id_minlimit,minlimit_value,id_dailylimit,dailylimit_value,id_submitbutton)
+        sTestStepReturnStatus = Web.SeleniumScript.Course_Settings_Time_Limit(id_minlimit,minlimit_value,id_dailylimit,dailylimit_value,id_submitbutton)
         print sTestStepReturnStatus
         return sTestStepReturnStatus  
     except:
@@ -77,7 +78,7 @@ def verify_text_message_by_class(dependency,step_data):
         first_data_set=step_data[0]
         message=first_data_set[0][0]
         expected_text =first_data_set[0][2]
-        sTestStepReturnStatus = SeleniumScript.Verify_Text_Message_By_Class(message,expected_text)
+        sTestStepReturnStatus = Web.SeleniumScript.Verify_Text_Message_By_Class(message,expected_text)
         print sTestStepReturnStatus
         return sTestStepReturnStatus 
     except:
@@ -86,7 +87,7 @@ def verify_text_message_by_class(dependency,step_data):
 
 def tear_down(dependency,step_data):
     try:        
-        sTestStepReturnStatus = SeleniumScript.Tear_Down()
+        sTestStepReturnStatus = Web.SeleniumScript.Tear_Down()
         print sTestStepReturnStatus
         return sTestStepReturnStatus
     except:
