5852b795fe5a0e11485bde6a5fe8071ebb63338e
Riz <hossain.iqbal@gmail.com>
2015-02-16 13:18:27 -0500

    - added few more functions for demo

--- Automationz/Framework_0.1/AutomationFW/TestRun/TestCases.py
+++ Automationz/Framework_0.1/AutomationFW/TestRun/TestCases.py
@@ -1,6 +1,14 @@
 import sys
 sys.path.append("..")
 
+'''
+To Run this suite, you need to:
+search: "CommonUtil."
+replace: "#CommonUtil."  in the file SeleniumScript
+and comment the import:
+#from CoreFrameWork import CommonUtil
+'''
+
 
 from Web import SeleniumScript
 
@@ -10,16 +18,16 @@ def Repurchase_the_course_when_the_final_exam_is_failed_for_third_time():
     print SeleniumScript.BrowserSelection('Firefox')
     print SeleniumScript.OpenLink('http://csdev-iqbal.jbldev.com/moodle/','csdev-iqbal')
     print SeleniumScript.Login('admin','R@1ndrops')
-    print SeleniumScript.Click_Element_By_Name_OR_ID('Turn editing on')
-    print SeleniumScript.Expand_Menu_By_Name_OR_ID('Site administration')
-    print SeleniumScript.Expand_Menu_By_Name_OR_ID('Plugins')
-    print SeleniumScript.Expand_Menu_By_Name_OR_ID('Local plugins')
-    print SeleniumScript.Expand_Menu_By_Name_OR_ID('ClickSafety')
-    print SeleniumScript.Click_Element_By_Name_OR_ID('Course settings')
-    print SeleniumScript.Click_Element_By_Name_OR_ID('Edit')
-    print SeleniumScript.Course_Settings_Time_Limit('id_minlimit', '600','id_dailylimit', '500','id_submitbutton')
-    print SeleniumScript.Verify_Text_Message_By_Class('message', 'Updated successfully')
-    print SeleniumScript.Tear_Down()
+    print SeleniumScript.Turn_Editing_On_OR_Off('on')
+#     print SeleniumScript.Expand_Menu_By_Name_OR_ID('Site administration')
+#     print SeleniumScript.Expand_Menu_By_Name_OR_ID('Plugins')
+#     print SeleniumScript.Expand_Menu_By_Name_OR_ID('Local plugins')
+#     print SeleniumScript.Expand_Menu_By_Name_OR_ID('ClickSafety')
+#     print SeleniumScript.Click_Element_By_Name_OR_ID('Course settings')
+#     print SeleniumScript.Click_Element_By_Name_OR_ID('Edit')
+#     print SeleniumScript.Course_Settings_Time_Limit('id_minlimit', '600','id_dailylimit', '500','id_submitbutton')
+#     print SeleniumScript.Verify_Text_Message_By_Class('message', 'Updated successfully')
+#     print SeleniumScript.Tear_Down()
 
 def Create_a_new_course():
     #make your test cases by re-using same steps
@@ -36,6 +44,7 @@ def Create_a_new_course():
     print SeleniumScript.Course_Settings_Time_Limit('id_minlimit', '600','id_dailylimit', '500','id_submitbutton')
     print SeleniumScript.Verify_Text_Message_By_Class('message', 'Updated successfully')
     print SeleniumScript.Tear_Down()
+
 def delete_a_course():
     print SeleniumScript.BrowserSelection('Firefox')
     print SeleniumScript.OpenLink('http://csdev-iqbal.jbldev.com/moodle/','csdev-iqbal')
@@ -54,7 +63,7 @@ def delete_a_course():
 
 def Test_Suite():
     #add additional test cases
-    #Repurchase_the_course_when_the_final_exam_is_failed_for_third_time()
-    delete_a_course()  
+    Repurchase_the_course_when_the_final_exam_is_failed_for_third_time()
+    #delete_a_course()  
 
 Test_Suite()
