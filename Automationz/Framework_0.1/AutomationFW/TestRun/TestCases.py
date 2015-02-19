a1e241578f95e9ce9229c1e6b1c703037c8bd2e2
Riz <hossain.iqbal@gmail.com>
2015-02-18 19:43:15 -0500

    - additional stuff added to automation functions

--- Automationz/Framework_0.1/AutomationFW/TestRun/TestCases.py
+++ Automationz/Framework_0.1/AutomationFW/TestRun/TestCases.py
@@ -19,21 +19,27 @@ def Repurchase_the_course_when_the_final_exam_is_failed_for_third_time():
     print SeleniumScript.OpenLink('http://csdev-iqbal.jbldev.com/moodle/','csdev-iqbal')
     print SeleniumScript.Login('admin','R@1ndrops')
     print SeleniumScript.Turn_Editing_On_OR_Off('on')
-#     print SeleniumScript.Expand_Menu_By_Name_OR_ID('Site administration')
-#     print SeleniumScript.Expand_Menu_By_Name_OR_ID('Plugins')
-#     print SeleniumScript.Expand_Menu_By_Name_OR_ID('Local plugins')
-#     print SeleniumScript.Expand_Menu_By_Name_OR_ID('ClickSafety')
-#     print SeleniumScript.Click_Element_By_Name_OR_ID('Course settings')
-#     print SeleniumScript.Click_Element_By_Name_OR_ID('Edit')
-#     print SeleniumScript.Course_Settings_Time_Limit('id_minlimit', '600','id_dailylimit', '500','id_submitbutton')
-#     print SeleniumScript.Verify_Text_Message_By_Class('message', 'Updated successfully')
-#     print SeleniumScript.Tear_Down()
+    print SeleniumScript.Expand_Menu_By_Name_OR_ID('Site administration')
+    print SeleniumScript.Expand_Menu_By_Name_OR_ID('Plugins')
+    print SeleniumScript.Expand_Menu_By_Name_OR_ID('Local plugins')
+    print SeleniumScript.Expand_Menu_By_Name_OR_ID('ClickSafety')
+    print SeleniumScript.Click_Element_By_Name_OR_ID('Course settings')
+    print SeleniumScript.Click_Element_By_Name_OR_ID('Edit')
+    print SeleniumScript.Course_Settings_Time_Limit('id_minlimit', '600','id_dailylimit', '500','id_submitbutton')
+    print SeleniumScript.Verify_Text_Message_By_Class('message', 'Updated successfully')
+    print SeleniumScript.Tear_Down()
 
-def Create_a_new_course():
+def Create_a_new_course(course, cleanup, from_begining):
     #make your test cases by re-using same steps
     print SeleniumScript.BrowserSelection('Firefox')
     print SeleniumScript.OpenLink('http://csdev-iqbal.jbldev.com/moodle/','csdev-iqbal')
     print SeleniumScript.Login('admin','R@1ndrops')
+    course_exists = SeleniumScript.Course_Exists(course)
+    if course_exists == "PASSED":
+        print "Course is already created"
+        print "Delete the course and recreate"
+        delete_a_course()
+    
     print SeleniumScript.Click_Element_By_Name_OR_ID('Turn editing on')
     print SeleniumScript.Expand_Menu_By_Name_OR_ID('Site administration')
     print SeleniumScript.Expand_Menu_By_Name_OR_ID('Plugins')
