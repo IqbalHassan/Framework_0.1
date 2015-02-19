18523ed4a03b6a2c806d81003d10dc3daccdd149
Matiur Rahman Minar <minar09.bd@gmail.com>
2015-02-19 18:16:16 +0600

    task creation and steps view feature show!

--- Automationz/Framework_0.1/WebServer/MySite/views.py
+++ Automationz/Framework_0.1/WebServer/MySite/views.py
@@ -731,7 +731,7 @@ def Steps_List(request):
                 itemPerPage, (current_page - 1) * itemPerPage)"""
 
             
-            query="select stepname,description,driver,steptype,automatable,stepenable,data_required,created_by,cv.value,stepfeature from test_steps_list tsl,config_values cv where tsl.team_id=cv.id::text and cv.type='Team' and project_id='"+project_id+"' "
+            query="select stepname,description,driver,steptype,automatable,stepenable,data_required,created_by,cv.value,pf.feature_path from test_steps_list tsl,config_values cv, product_features pf where tsl.team_id=cv.id::text and cv.type='Team' and project_id='"+project_id+"' and pf.feature_id::text=tsl.stepfeature "
             steps_list=DB.GetData(Conn, query, False)
             count = len(steps_list)
             
