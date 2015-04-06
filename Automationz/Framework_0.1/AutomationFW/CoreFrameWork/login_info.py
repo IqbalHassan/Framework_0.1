f3773e83c9b9b7c12fa151a44a5e247d77d3b771
Raju Ahmed <shetu2153@gmail.com>
2015-04-06 11:14:07 +0600

    login_info.py is improved. Enviroment settings is getting done from the GLobal.py file.

--- Automationz/Framework_0.1/AutomationFW/CoreFrameWork/login_info.py
+++ Automationz/Framework_0.1/AutomationFW/CoreFrameWork/login_info.py
@@ -1,11 +1,17 @@
 import sys
+import Global
 sys.path.append("..")
-
+def get_ip(environment):
+    if environment=='Production':
+        return '135.23.123.206'
+    if environment=='Test':
+        return '127.0.0.1'
+    
 username='iqbal_hassan'
 password='iqbal_hassan'
 project='AscendLearning'
 team='ClickSafety'
-server='135.23.123.206'
+server=get_ip(Global.Environment)
 port=5432
 
 
@@ -14,3 +20,5 @@ port=5432
 database_name='postgres'
 superuser='postgres'
 super_password='password'
+
+
