942a7e79e17e413effaee9688625ff09f5502f71
Taitalus <shetu2153@gmail.com>
2015-06-10 15:06:18 +0600

    Settings is modified.

--- Automationz/Framework_0.1/WebServer/settings.py
+++ Automationz/Framework_0.1/WebServer/settings.py
@@ -266,7 +266,7 @@ WSGI_APPLICATION = 'WebServer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
     }
 }
 """
