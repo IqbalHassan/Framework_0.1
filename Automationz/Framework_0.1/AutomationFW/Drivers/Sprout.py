2d3d7bd9a88e6e0600a6a65c8446b4be50161e5b
Taitalus <shetu2153@gmail.com>
2015-06-12 22:05:20 +0600

    No more today

--- Automationz/Framework_0.1/AutomationFW/Drivers/Sprout.py
+++ Automationz/Framework_0.1/AutomationFW/Drivers/Sprout.py
@@ -79,8 +79,17 @@ def tear_down(dependency,step_data,temp_q):
         return "Failed"
 
 class Sprout(FunkLoadTestCase):
+    def setUp(self):
+        self.server_url=self.conf_get('main','url')
     def performance_test_case(self):
-        print "hello world"
+        server_url=self.server_url
+        res = self.get(server_url, description='Get url')
+        self.assertEqual(res.code, 200)
+        #self.assertEqual(res.body, "Hello World")
+        res=self.get(server_url+'/Home/Login/', params={'username':self.conf_get('performance_test_case','username'),'password':self.conf_get('performance_test_case','password')})
+        self.assertEqual(res.code, 200)
+        res=self.get(server_url+'/Home/Results/')
+        print res.code
 
 from funkload.BenchRunner import parse_sys_args, load_unittest
 from AutomationFW.CoreFrameWork.ConfigFileGenerator import write_config_file
@@ -93,7 +102,7 @@ def performance_test_case(dependency_list,steps_data,temp_q):
     #generate the config file from here
     default_configs={
         'bench':[('cycles','5:10:20:50'),('duration',10),('startup_delay',0.01),('sleep_time',0.01),('cycle_time',1),('log_to','console file'),('sleep_time_min',0),('sleep_time_max',0.5),('log_path','Log/funkload-bench.log'),('result_path','Result/funkload-bench.xml')],
-        'main': [('title','Performance Test Case in '+klass),('description','This will run a Performance Test case in '+klass+ ' module'),('url','offical link of the owner of '+klass+' module')]
+        'main': [('title','Performance Test Case in '+klass),('description','This will run a Performance Test case in '+klass+ ' module'),('url','127.0.0.1:8000')]
     }
     write_config_file(full_file_name,method,dependency_list[1],steps_data,default_configs,klass)
     test = load_unittest(module_name, klass,mmn_encode(method, 0, 0, 0), options)
