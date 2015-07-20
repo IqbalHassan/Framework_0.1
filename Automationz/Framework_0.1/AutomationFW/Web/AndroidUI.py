9ab552291daf6debdc0eb2bce6a4ef1513f2025e
Taitalus <shetu2153@gmail.com>
2015-07-20 12:18:56 +0600

    Call holdin is done using ui automator is done in samsung.

--- Automationz/Framework_0.1/AutomationFW/Web/AndroidUI.py
+++ Automationz/Framework_0.1/AutomationFW/Web/AndroidUI.py
@@ -8,7 +8,7 @@ class cell_phone():
         for each in d:
             setattr(self,each,d[each])
     def rep_(self):
-        return "id:%s, number:%s"%(self.id,self.cell)
+        return "id:%s, number:%s,brand:%s"%(self.id,self.cell,self.brand)
 def make_phone_call(caller,callee):
     adb_command="adb -s %s shell am start -a android.intent.action.CALL -d tel:%s"%(caller.id,callee.cell)
     print adb_command
@@ -39,34 +39,64 @@ def reject_call(callee):
     print "rejecting the call"
     os.system(adb_command)
 def hold_call(callee):
+    print callee.brand
     d=Device(callee.id,adb_server_host='127.0.0.1',adb_server_port=5037)
-    print "finding the button"
-    d(resourceId='com.asus.asusincallui:id/holdButton').click()
-    d.screenshot('incall.png')
-    print "pressed the button"
+    if callee.brand=='asus':
+        print "finding the button"
+        d(resourceId='com.asus.asusincallui:id/holdButton').click()
+        d.screenshot('incall.png')
+        print "pressed the button"
+    if callee.brand=='samsung':
+        print "found samsung"
+        d(resourceId='com.android.incallui:id/onScreenMenuBtn').click()
+        print "pressed the menu button"
+        #d(resourceId='com.android.incallui:id/holdButton').click()
+        d(text="Hold").click()
+        print "pressed the hold button"
+def check_connectivity_first(devices):
+    id_list=[x.id for x in devices]
+    #print id_list
+    adb_commnd="adb devices"
+    s=subprocess.Popen(adb_commnd,shell=False,stdout=subprocess.PIPE)
+    p=s.communicate()[0]
+    p=p.split("List of devices attached")[1].strip().split("\r\n")
+    attached_device=[]
+    for each in p:
+        d=each.split('\tdevice')[0].strip()
+        attached_device.append(d)
+    un_attached=list(set(id_list)-set(attached_device))
+    if un_attached:
+        print ",".join(un_attached)+' not attached by usb'
+        return False
+    else:
+        print "All devices are attached"
+        return True
 def main():
     d=[{
         'id':'F4AZCY05A885',
-        'cell':'01719267494'
+        'cell':'01719267494',
+        'brand':'asus',
     },{
         'id':'4d00f8cee4d74200',
-        'cell':'01721770326'
-    },{
-        'id':'',
-        'cell':'01684667279'
-    }]
+        'cell':'01721770326',
+        'brand':'samsung',
+    },]
     device_list=[]
     for each in d:
         device_list.append(cell_phone(each))
-
-    make_phone_call(device_list[1],device_list[0])
-    if(look_for_signal(device_list[0],30)):
-        receive_call(device_list[0])
-    time.sleep(5)
-    hold_call(device_list[0])
-    time.sleep(5)
-    make_phone_call(device_list[0],device_list[2])
-    time.sleep(5)
-    reject_call(device_list[1])
+    print device_list
+    for e in device_list:
+        print e.rep_()
+    if check_connectivity_first(device_list):
+        make_phone_call(device_list[1],device_list[0])
+        if(look_for_signal(device_list[0],30)):
+            receive_call(device_list[0])
+        time.sleep(5)
+        hold_call(device_list[1])
+        time.sleep(5)
+        reject_call(device_list[0])
+    else:
+        print "Check Connectivity First"
+        return False
 if __name__=='__main__':
     main()
